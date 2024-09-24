d1ff5b2223be4a8ba322adfcbd085a3959ee7558
AdBinay <binayadh77@gmail.com>
2024-09-24 11:42:57 +0545

    package_work

---
+++ chapter1.2_Lab/MathOprn.java
@@ -0,0 +1,9 @@
+import Operation.ArtOprn;
+
+public class MathOprn {
+    public static void main(String[] args) {
+        ArtOprn a = new ArtOprn();
+        System.out.println(a.add(4,5));
+        System.out.println(a.sub(5,2));
+    }
+}
---
+++ chapter1.2_Lab/Operation/ArtOprn.java
@@ -0,0 +1,12 @@
+package Operation;
+
+public class ArtOprn{
+    public double add(double a, double b){
+        return (a+b);
+    }
+    public double sub(double a, double b){
+        return (a-b);
+    }
+}
+
+
